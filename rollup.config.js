b2d92d0720941056858b86b8ed2ecec193374de3
Jay Gao <gaoljie@gmail.com>
2020-09-09 22:37:32 -0700

    add scss processor

--- rollup.config.js
+++ rollup.config.js
@@ -1,13 +1,30 @@
 import ts from "@wessberg/rollup-plugin-ts";
 import resolve from "@rollup/plugin-node-resolve";
+import postcss from "rollup-plugin-postcss";
+import autoprefixer from "autoprefixer";
 
-export default {
-  input: "src/index.tsx",
-  output: {
-    file: "lib/index.js",
-    format: "cjs",
-    exports: "default"
+export default [
+  {
+    input: "src/index.tsx",
+    output: {
+      file: "lib/index.js",
+      format: "cjs",
+      exports: "default"
+    },
+    plugins: [ts(), resolve()],
+    external: ["react", "react-dom"]
   },
-  plugins: [ts(), resolve()],
-  external: ["react", "react-dom"]
-};
+  {
+    input: "src/slider.scss",
+    output: {
+      file: "lib/slider.min.css"
+    },
+    plugins: [
+      postcss({
+        extract: true,
+        minimize: true,
+        plugins: [autoprefixer()]
+      })
+    ]
+  }
+];
